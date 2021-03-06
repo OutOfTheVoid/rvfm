e675238e61a979887599d1653ca294eaf6f7a30e
liam <liam.tab@gmail.com>
2021-03-06 02:20:09 -0800

    rename tests and remove old test

--- test/test1/build.sh
+++
@@ -1,3 +0,0 @@
-riscv32-unknown-elf-g++ test/test1/test1.s -ffreestanding -c -o test/test1/out.o -march=rv32im -mno-relax -mexplicit-relocs
-riscv32-unknown-elf-ld test/test1/out.o -T test/test1/link.ld -o test/test1/out.elf
-riscv32-unknown-elf-elf2bin --input test/test1/out.elf --output test/test1/out.bin 
